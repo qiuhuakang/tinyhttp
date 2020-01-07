76ce371b8f7c1f98d287b7d999fdf19543582dee
qiuhuakang <846815707@qq.com>
2020-01-07 08:36:45 +0000

    load

--- http.cpp
+++ http.cpp
@@ -75,8 +75,37 @@ void accept_request(void *arg)
 	}
 	url[i] = '\0';
 
-	if (strcasecmp(method,
+	if (strcasecmp(method, "GET") == 0)
+	{
+		query_string = url;
+		while ((*query_string != '?') && (*query_string != '\0'))
+			query_string++;
+		if (*query_string = '?')
+		{
+			cgi = 1;
+			*query_string = '\0';
+			query_string++;
+		}
+	}
 
+	sprintf(path, "htdocs%s", url);
+	if (path[strlen(path) - 1] == '/')
+		strcat(path, "index.html");
+	if (stat(path, &st) == -1){
+		while ((numchars > 0) && strcmp("\n", buf))
+			numchars = get_line(client, buf, sizeof(buf));
+		not_found(client);
+	}
+	else
+	{
+		if ((st.st_mode & S_IFMT) == S_IFDIR)
+			strcat(path, "/index.html");
+		if ((st.st_mode & S_IXUSR) || 
+			(st.st_mode & S_IXGRP) ||
+			(st.st_mode & S_IXOTH)   )
+			cgi = 1;
+		if (!cgi)
+			serve_file(client, path);
 
 void bad_request(int client)
 {
@@ -92,6 +121,32 @@ void bad_request(int client)
 	send(client, buf, sizeof(buf), 0);
 	sprintf(buf, "such as a POST without a Content_Length.\r\n");
 	send(client, buf, sizeof(buf), 0);
+}
+
+void cat(int client, FILE *resource)
+{
+	char buf[1024];
+
+	fgets(buf, sizeof(buf), resource);
+	while(!feof(resource))
+	{
+		send(client, buf, strlen(buf), 0);
+		fgets(buf, sizeof(buf), resource);
+	}
+}
+
+void cannot_execute(int client)
+{
+	char buf[1024];
+
+	sprintf(buf, "HTTP/1.0 500 Internal Server Error\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "Content-type: text/html\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "<P>Error prohibited CGI execution.\r\n");
+	send(client, buf, strlen(buf), 0);
 }
 
 void execute_cgi(int client, const char *path,
@@ -126,7 +181,63 @@ void execute_cgi(int client, const char *path,
 			return;
 		}
 	}
+	else /*HEAD or other*/
+	{
+	}
+	
+	if(pipe(cgi_output) < 0){
+		cannot_execute(client);
+		return;
+	}
+	if(pipe(cgi_input) < 0){
+		cannot_execute(client);
+		return;
+	}
+	if((pid = fork()) < 0){
+		cannot_execute(client);
+		return;
+	}
+	sprintf(buf, "HTTP/1.0 200 OK\r\n");
+	send(client, buf, strlen(buf), 0);
+	if(pid == 0) /* child:CGI sript */
+	{
+		char meth_env[255];
+		char query_env[255];
+		char length_env[255];
 
+		dup2(cgi_output[1], STDOUT);
+		dup2(cgi_input[0],  STDIN);
+		close(cgi_output[0]);
+		close(cgi_input[1]);
+		sprintf(meth_env, "REQUEST_METHOD=%s", method);
+		putenv(meth_env);
+		if(strcasecmp(method, "GET") == 0){
+			sprintf(query_env, "QUERY_STRING=%s", query_string);
+			putenv(query_env);
+		}
+		else{ /* POST */
+			sprint(length_env, "LENGTH_STRING=%s", length_string);
+			putenv(length_env);
+		}
+		execl(path, NULL);
+		exit(0);
+	}else{ /* parent * /
+		close(cgi_output[1]);
+		close(cgi_input[0]);
+		if (strcasecmp(method, "POST") == 0)
+			for (i = 0; i<content_length; i++){
+				recv(client, &c, 1, 0);
+				write(cgi_input[1], &c, 1);
+			}
+		while (read(cgi_output[0], &c, 1) > 0)
+			send(client, &c, 1, 0);
+
+		close(cgi_output[0]);
+		close(cgi_input[1]);
+		waitpid(pid, &status, 0);
+	}
+}		
+		
 int get_line(int sock, char *buf, int size)
 {
 	int  i = 0;
@@ -156,6 +267,56 @@ int get_line(int sock, char *buf, int size)
 	buf[i] = '\0';
 
 	return(i);
+}
+
+void headers(int client, const char *filename)
+{
+	char buf[1024];
+	(void)filename; 
+
+void not_found(int client)
+{
+	char buf[1024];
+		
+	sprintf(buf, "HTTP/1.0 400 NOT FOUND\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, SERVER_STRING);
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "Content-type: text/html\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "\r\n");
+	send(cllent, buf, strlen(buf), 0);
+	sprintf(buf, "<HTML><TITLE>Not Found</TITLE>\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "<BODY><P>The serve could not fulfill\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "your request because the resource specified\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "is unavailable or nonexistent.\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "</BODY></HTML>\r\n");
+	send(client, buf, strlen(buf), 0);
+}
+
+void serve_file(int client, const char *filename)
+{
+	FILE *resource = NULL;
+	int numchars = 1;
+	char buf[1024];
+
+	buf[0] = 'A'; buf[1] ='\0';
+	while ((numchars > 0) && strcmp("\n", buf))
+		numchars = get_line(client, buf, sizeof(buf));
+
+	resource = fopen(filename, "r");
+	if (resource == NULL)
+		not_found(client);
+	else
+	{
+		headers(client, filename);
+		cat(client, resource);
+	}
+	fclose(resource);
 }
 
 void error_die(const char *sc)
